fa936411c20ee10b121f407ed492b0e60144b787
angelo <angelods18@gmail.com>
2022-09-06 00:06:14 +0200

    download media api

--- src/main/java/it/angelods/ac/webserver/controller/MediaController.java
+++ src/main/java/it/angelods/ac/webserver/controller/MediaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import it.angelods.ac.webserver.document.ResourceStream;
 import it.angelods.ac.webserver.service.MediaService;
 
 @RestController
@@ -20,10 +21,12 @@ public class MediaController {
 	@Autowired
 	private MediaService mediaService;
 	
-	@GetMapping(value="{mediaId}", produces = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
+	@GetMapping(value="{mediaId}", produces = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE,
+			MediaType.APPLICATION_PDF_VALUE})
 	public byte[] getMedia(@PathVariable("mediaId") String mediaId) {
 		try {
-			return mediaService.getMedia(mediaId).getStream().readAllBytes();
+			ResourceStream rs = mediaService.getMedia(mediaId);
+			return rs.getStream().readAllBytes();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			return null;
